d37a7048f50a1c837879843188459fa04b1c3bb4
dsch <davidschreiterprogramming@gmail.com>
2023-07-19 11:29:50 -0700

    removal of Chakra, conversion to custom checkbox

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,55 +1,49 @@
-import { Stack, Button, CheckboxGroup, Checkbox } from "@chakra-ui/react";
 import { useState } from "react";
+import Checkbox from "./Checkbox";
 
 function Filter({ setActiveFiltersList }) {
-  const filterOptions = ["all", "weights", "cardio"];
+	const filterOptions = ["all", "weights", "cardio"];
 
-  const [checkedState, setCheckedState] = useState(
-    new Array(filterOptions.length).fill(false)
-  );
+	const [checkedState, setCheckedState] = useState(
+		new Array(filterOptions.length).fill(false)
+	);
 
-  const handleOnChange = (position) => {
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item
-    );
+	const handleOnChange = (position) => {
+		const updatedCheckedState = checkedState.map((item, index) =>
+			index === position ? !item : item
+		);
 
-    setCheckedState(updatedCheckedState);
-  };
+		setCheckedState(updatedCheckedState);
+	};
 
-  const renderedCheckbox = filterOptions.map((option, index) => {
-    return (
-      <Checkbox
-        key={index}
-        value={option}
-        checked={checkedState[index]}
-        onChange={() => handleOnChange(index)}
-      >
-        {option}
-      </Checkbox>
-    );
-  });
+	const handleFilter = (event) => {
+		event.preventDefault();
+		const checkedBoxes = renderedCheckbox.filter(
+			(box) => box.props.checked
+		);
 
-  const handleFilter = (event) => {
-    event.preventDefault();
-    const checkedBoxes = renderedCheckbox.filter((box) => box.props.checked);
-    const filters = checkedBoxes.map((box) => box.props.value);
-    setActiveFiltersList(filters);
-  };
+		const filters = checkedBoxes.map((box) => box.props.value);
 
-  return (
-    <form onSubmit={handleFilter}>
-      <Stack direction={"row"}>
-        <CheckboxGroup colorScheme="green" defaultValue={["naruto", "kakashi"]}>
-          <Stack spacing={[1, 5]} direction={["column", "row"]}>
-            {renderedCheckbox}
-          </Stack>
-        </CheckboxGroup>
-        <Button type="submit" colorScheme="blue">
-          Filter Library
-        </Button>
-      </Stack>
-    </form>
-  );
+		setActiveFiltersList(filters);
+	};
+
+	const renderedCheckbox = filterOptions.map((option, index) => {
+		return (
+			<Checkbox
+				key={index}
+				value={option}
+				checked={checkedState[index]}
+				onChange={() => handleOnChange(index)}
+			/>
+		);
+	});
+
+	return (
+		<form onSubmit={handleFilter}>
+			{renderedCheckbox}
+			<button type="submit">Filter Library</button>
+		</form>
+	);
 }
 
 export default Filter;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<ChakraProvider>
-		<App />
-	</ChakraProvider>
-);
+root.render(<App />);
