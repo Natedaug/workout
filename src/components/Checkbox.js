fae95a01ce551aafb1cb732c6aa31c455507ea16
Nathan Kroenung <nathanakroenung@gmail.com>
2023-07-19 13:44:42 -0700

    removed filter btn

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,12 +1,12 @@
 function Checkbox({ value, ...rest }) {
-	return (
-		<div className="checkbox-wrapper">
-			<label>
-				<input type="checkbox" name={value} value={value} {...rest} />
-				<span>{value}</span>
-			</label>
-		</div>
-	);
+  return (
+    <span className="checkbox-wrapper">
+      <label>
+        <input type="checkbox" name={value} value={value} {...rest} />
+        <span>{value}</span>
+      </label>
+    </span>
+  );
 }
 
 export default Checkbox;
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,49 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Checkbox from "./Checkbox";
 
 function Filter({ setActiveFiltersList }) {
-	const filterOptions = ["all", "weights", "cardio"];
+  const filterOptions = ["all", "weights", "cardio"];
 
-	const [checkedState, setCheckedState] = useState(
-		new Array(filterOptions.length).fill(false)
-	);
+  const [checkedState, setCheckedState] = useState(
+    new Array(filterOptions.length).fill(false)
+  );
 
-	const handleOnChange = (position) => {
-		const updatedCheckedState = checkedState.map((item, index) =>
-			index === position ? !item : item
-		);
+  useEffect(() => {
+    //handle active filters state
+    const checkedBoxes = renderedCheckbox.filter((box) => box.props.checked);
+    const filters = checkedBoxes.map((box) => box.props.value);
+    setActiveFiltersList(filters);
+  }, [checkedState]);
 
-		setCheckedState(updatedCheckedState);
-	};
+  const handleCheckbox = (position) => {
+    //handle checked state
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+  };
 
-	const handleFilter = (event) => {
-		event.preventDefault();
-		const checkedBoxes = renderedCheckbox.filter(
-			(box) => box.props.checked
-		);
+  const renderedCheckbox = filterOptions.map((option, index) => {
+    return (
+      <Checkbox
+        key={index}
+        value={option}
+        checked={checkedState[index]}
+        onChange={() => handleCheckbox(index)}
+      />
+    );
+  });
 
-		const filters = checkedBoxes.map((box) => box.props.value);
-
-		setActiveFiltersList(filters);
-	};
-
-	const renderedCheckbox = filterOptions.map((option, index) => {
-		return (
-			<Checkbox
-				key={index}
-				value={option}
-				checked={checkedState[index]}
-				onChange={() => handleOnChange(index)}
-			/>
-		);
-	});
-
-	return (
-		<form onSubmit={handleFilter}>
-			{renderedCheckbox}
-			<button type="submit">Filter Library</button>
-		</form>
-	);
+  return <form>{renderedCheckbox}</form>;
 }
 
 export default Filter;
