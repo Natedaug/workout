d9d621edd9422f2fdc492ddc410d8757ddae9858
dsch <davidschreiterprogramming@gmail.com>
2023-07-21 10:45:43 -0700

    basic error handling for routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
-import HomePage from "./pages/HomePage";
+import LibraryPage from "./pages/LibraryPage";
+import ErrorPage from "./pages/ErrorPage";
 import UserWorkoutPage from "./pages/UserWorkoutPage";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <RootLayout />,
+		errorElement: <ErrorPage />,
 		children: [
-			{ path: "/", element: <HomePage /> },
+			{ path: "/", element: <LibraryPage /> },
 			{ path: "/userWorkout", element: <UserWorkoutPage /> },
 		],
 	},
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+function Error(props) {
+	return (
+		<>
+			<Navbar />
+			<div>An error Occurred</div>
+		</>
+	);
+}
+
+export default Error;
