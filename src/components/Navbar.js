7e8e41c5b741c75d4c8c362e28eab256500a9c5b
dsch <davidschreiterprogramming@gmail.com>
2023-10-06 14:44:14 -0700

    added nav color, centered layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,15 +7,17 @@ function Navbar() {
 	];
 
 	const renderedRoutes = routes.map((route, i) => (
-		<li key={i}>
-			<Link to={route.path}>{route.label}</Link>
+		<li key={i} className="pr-8">
+			<Link to={route.path}>
+				<span className="hover:text-white">{route.label}</span>
+			</Link>
 		</li>
 	));
 
 	return (
 		<header>
-			<nav>
-				<ul>{renderedRoutes}</ul>
+			<nav className="bg-indigo-700 text-indigo-100 text-lg font-semibold py-2 pl-150">
+				<ul className="flex justify-center">{renderedRoutes}</ul>
 			</nav>
 		</header>
 	);
