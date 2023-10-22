3660f33167e9deffd553da7b92b6f9155f1df0a2
dsch <davidschreiterprogramming@gmail.com>
2023-10-22 12:55:42 -0700

    changed Button default styling approach

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,7 @@
 const Button = ({ className, onClick, children, ...rest }) => {
-	const defaultStyling = "rounded-full ml-4 border-2 px-4 mb-4";
-
 	return (
 		<button
-			className={`${defaultStyling}${" "}${className}`}
+			className={`rounded-full ml-4 border-2 px-4 mb-4 ${" "}${className}`}
 			onClick={onClick}
 			{...rest}
 		>
