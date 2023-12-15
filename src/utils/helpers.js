c97bb3a97daae4d57bf952c739ad3e31ebb3ad8e
dsch <davidschreiterprogramming@gmail.com>
2023-12-15 13:12:04 -0800

    temp alert message added to error

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,6 @@
 export const logError = (error) => {
 	console.error("!!! An error has occurred: ", error);
+
+	// to replace with modal, or ErrorPage
+	alert("An error has occurred, unable to complete your action.");
 };
