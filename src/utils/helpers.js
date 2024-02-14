f6e4ed5979c34f47d685153f30680e60771ca4a4
dsch <davidschreiterprogramming@gmail.com>
2024-02-14 13:50:49 -0800

    refine logError function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,10 @@
-export const logError = (error) => {
-	console.error("!!! An error has occurred: ", error);
+export const logError = (response) => {
+	const errorMessage = `An error has occurred: ${response.message}`;
 
-	// to replace with modal, or ErrorPage
-	alert("An error has occurred, unable to complete your action.");
+	const httpStatus = response.status
+		? `HTTP status code: ${response.status}`
+		: "No HTTP status code available";
+
+	console.error(`errorMessage: ${errorMessage}`);
+	console.error(`httpStatus: ${httpStatus}`);
 };
